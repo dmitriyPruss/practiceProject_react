cf6cc5bf225cc7e3dd7ec3c94f8d2ecdf7709762
Dmitriy <dmitriy.tackle@gmail.com>
2021-09-05 15:56:01 +0300

    add PricingPage&styles

--- client/src/pages/PricingPage/index.jsx
+++ client/src/pages/PricingPage/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import prices from './prices.json';
+import styles from './PricingPage.module.scss';
 
 function PricingPage () {
   return (
     <>
       <Header />
-      <article></article>
+      <article className={styles.pricesContainer}>
+        {prices.map(
+          ({
+            colorName,
+            description,
+            price: { amount, currency },
+            options,
+            backgroundColor,
+            linkAddress,
+          }) => (
+            <article className={styles.priceContainer}>
+              <section
+                style={{ border: `10px solid ${backgroundColor}` }}
+                className={styles.priceElemContainer}
+              >
+                <h3 style={{ color: backgroundColor }}>{colorName}</h3>
+                <p>{description}</p>
+                <div style={{ color: backgroundColor }}>
+                  {currency}
+                  {amount}
+                </div>
+              </section>
+              <ul>
+                {options.map(option => (
+                  <li>
+                    <span>{option}</span>
+                    <hr />
+                  </li>
+                ))}
+              </ul>
+              <div
+                style={{ backgroundColor: backgroundColor }}
+                className={styles.priceButton}
+              >
+                <i class='fas fa-check'></i>
+                <a href={linkAddress}>Start</a>
+              </div>
+            </article>
+          )
+        )}
+      </article>
       <Footer />
     </>
   );
